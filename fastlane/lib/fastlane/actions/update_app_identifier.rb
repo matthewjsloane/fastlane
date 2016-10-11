68c07b06772dbcf41ee0f2dae916fb2ea304396c
matthewjsloane <msloane@thoughtworks.com>
2016-10-11 11:52:35 -0700

    temp commit

--- fastlane/lib/fastlane/actions/update_app_identifier.rb
+++ fastlane/lib/fastlane/actions/update_app_identifier.rb
@@ -26,10 +26,14 @@ module Fastlane
           configs = configs.select { |obj| obj.build_settings[info_plist_key] == params[:plist_path] }
           UI.user_error!("Xcodeproj doesn't have configuration with info plist #{params[:plist_path]}.") unless configs.count > 0
 
-          # For each of the build configurations, set app identifier
-          configs.each do |c|
-            c.build_settings[identifier_key] = params[:app_identifier]
-          end
+          if params[:configuration]
+            config = configs.find { |obj| obj.build_settings[identifier_key] == params[:app_identifier] } and UI.user_error!("Configuration #{params[:configuration} does not exist") unless !config.nil?
+            config.build_settings[identifier_key] == params[:app_identifier]
+          else
+            # For each of the build configurations, set app identifier
+            configs.each do |c|
+              c.build_settings[identifier_key] = params[:app_identifier]
+            end
 
           # Write changes to the file
           project.save
@@ -82,7 +86,11 @@ module Fastlane
           FastlaneCore::ConfigItem.new(key: :app_identifier,
                                        env_name: 'FL_UPDATE_APP_IDENTIFIER',
                                        description: 'The app Identifier you want to set',
-                                       default_value: ENV['PRODUCE_APP_IDENTIFIER'] || CredentialsManager::AppfileConfig.try_fetch_value(:app_identifier))
+                                       default_value: ENV['PRODUCE_APP_IDENTIFIER'] || CredentialsManager::AppfileConfig.try_fetch_value(:app_identifier)),
+          FastlaneCore::ConfigItem.new(key: :configuration,
+                                       env_name: 'FL_UPDATE_APP_IDENTIFIER_CONFIGURATION',
+                                       description: 'The configuration you want to set. Defaults to all.',
+                                       optional: true)
         ]
       end
 
